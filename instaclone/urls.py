7be7c70ef4820f08d7e11aedcc0ee2adef94dc9e
tonyishangu <tonyishangu@gmail.com>
2020-10-19 01:15:04 +0300

    add login content

--- instaclone/urls.py
+++ instaclone/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url,include
 from django.contrib import admin
-from django.urls import path
+from django.contrib.auth import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('^admin/', admin.site.urls),
+    url('^',include('pics.urls')),
+    url('^accounts/', include('registration.backends.simple.urls')),
+    url('^tinymce/', include('tinymce.urls')),
+    url('^logout', views.logout, {"next_page": '/'})
 ]
